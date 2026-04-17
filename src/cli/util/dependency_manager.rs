c5797dcea6ae5fb179fe20b210dacf7d066c6e92
b1t-ninja <nrueckner@outlook.de>
2026-04-17 18:12:50 +0200

    added more funcs to dependency manager

--- src/cli/util/dependency_manager.rs
+++ src/cli/util/dependency_manager.rs
@@ -1,13 +1,30 @@
-use std::{fs, path::PathBuf};
+use std::{env, fmt::Result, fs, path::PathBuf};
+
+use serde::de::Error;
+
+use crate::cli::util::dependency::Dependency;
 
 pub struct DependencyManager {
-  cwd: PathBuf,
+  path_to_package_swift_file: PathBuf,
 }
 
 impl DependencyManager {
-  pub fn read_package_swift_file(&self) -> String {
-    let path = self.cwd.join("Package.swift");
-    fs::read_to_string(path).expect("Failed to read the file")
+  fn read_package_swift_file(&self) -> String {
+    fs::read_to_string(&self.path_to_package_swift_file).expect("Failed to read the file")
+  }
+
+  fn write_package_swift_file(&self, new_content: String) {
+    fs::write(&self.path_to_package_swift_file, new_content).expect("Failed to write to file")
+  }
+}
+
+impl DependencyManager {
+  pub fn new() -> DependencyManager {
+    Self {
+      path_to_package_swift_file: env::current_dir()
+        .expect("Failed to grab the current dir")
+        .join("Package.swift"),
+    }
   }
 }
 
